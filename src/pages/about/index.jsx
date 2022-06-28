782b4fe1823536f2899e7c164cd4d2d540fade3d
Maria Letícia <maria12leticia31@gmail.com>
2022-06-28 16:03:38 -0300

    Add about page
    Co-authored-by: Vitória Carla Costa de Lira <assessoriavitorialira@gmail.com>
    Co-authored-by: Beatriz Fontes <beatrizgfontes@users.noreply.github.com>
    Co-authored-by: Arthur V.B.S. <ArthurVBS@users.noreply.github.com>

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 import Footer from '../../components/footer'
 import Header from '../../components/header'
+import Astronaut from '../../assets/astronaut-about.png'
+import { Container, LeftBox, Text, Image, Title } from './styles'
 
 const About = () => {
   return (
     <>
       <Header />
-      <p>Esta é a página About</p>
+
+      <Container>
+
+        <LeftBox>
+          <Title>Quem Somos</Title>
+          <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborumre et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</Text>
+        </LeftBox>
+
+        <Image src={Astronaut} />
+
+      </Container>
+
       <Footer />
     </>
   )
---
+++ src/pages/about/styles.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  gap: 50px;
+  padding: 5%;
+  background-image: linear-gradient(to left, #161B22, #00A6ED 70%);
+  color: ${props => props.theme.colors.text};
+
+  @media screen and (max-width: 728px){
+    flex-direction:column;
+  }
+`
+
+export const Image = styled.img`
+  padding-left: 5%;
+`
+
+export const LeftBox = styled.div`
+  width: 50%;
+
+  @media screen and (max-width: 728px){
+    width: 100%;
+  }
+`
+
+export const Title = styled.h1`
+`
+
+export const Text = styled.p`
+  text-align: justify;
+  padding-top: 16px;
+  font-size: 1.5em;
+`
