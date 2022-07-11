ed093e0baca8aa9d2827b89b75f393de2430d003
ArthurVBS <arthurvini2703@gmail.com>
2022-07-11 10:11:34 -0300

    Add initial backend code

---
+++ backend/src/main/java/com/app/infinity/InfinityApplication.java
@@ -0,0 +1,13 @@
+package com.app.infinity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfinityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfinityApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/app/infinity/InfinityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.infinity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfinityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
