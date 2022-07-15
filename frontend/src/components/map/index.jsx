5c1a8975f0159d5cd7cc08bf3179055b5824530d
Thiago Vilela <thiago.vilela@outlook.com.br>
2022-07-15 20:11:12 -0300

    Update Contact Page

--- frontend/src/components/map/index.jsx
+++ frontend/src/components/map/index.jsx
@@ -5,20 +5,16 @@ const Map = () => {
   return (
     <Container>
       <address>
-        <a href="https://thiagomvilela.github.io/Infinity/" target="_blank">
-          Infinity
-        </a>
-        <br />
-        Maranguape I, CEP: 53441-601
-        <br />
-        Paulista-PE
+        <p>Entre em Contato por E-mail: <a href="mailto:infinity.ifpe@gmail.com">Clique aqui 📧</a></p>
+        <p> Visite nossa Página: <a href="https://thiagomvilela.github.io/Infinity/" target="_blank">Infinity♾️</a></p>
+        <p> Instituto Federal de Pernambuco: <a href="https://www.ifpe.edu.br/campus/paulista" target="_blank">IFPE (Campus Paulista)</a></p>
       </address>
 
       <hr />
-      <h4>Endereço:</h4>
+      <h5>Endereço:</h5>
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.515056446582!2d-34.861024148546555!3d-7.94560588132668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab3dbf4a9cd5f1%3A0x45bf1641ad29a799!2sIFPE-%20Intituto%20Federal%20De%20Pernambuco%20%2C%20Paulista!5e0!3m2!1spt-BR!2sbr!4v1650408729027!5m2!1spt-BR!2sbr"
-        width="600"
+        width="800"
         height="450"
         allowFullScreen=""
         loading="lazy"
