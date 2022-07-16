f1853d0899413ccd4e84c0706c1f064b0d6e3605
Thiago Vilela <thiago.vilela@outlook.com.br>
2022-07-16 12:43:07 -0300

    Update contact page and starting deploy backend

--- frontend/src/components/map/index.jsx
+++ frontend/src/components/map/index.jsx
@@ -1,26 +1,85 @@
 import React from 'react'
-import { Container } from './styles'
+import qrcode from '../../assets/qrcode.png'
+import googlemaps1 from '../../assets/googlemaps1.png'
+import { Image, Title, Text, Div, Div2} from './styles'
+import { Row, Col, Container } from 'react-bootstrap'
 
 const Map = () => {
   return (
-    <Container>
-      <address>
-        <p>Entre em Contato por E-mail: <a href="mailto:infinity.ifpe@gmail.com">Clique aqui 📧</a></p>
-        <p> Visite nossa Página: <a href="https://thiagomvilela.github.io/Infinity/" target="_blank">Infinity♾️</a></p>
-        <p> Instituto Federal de Pernambuco: <a href="https://www.ifpe.edu.br/campus/paulista" target="_blank">IFPE (Campus Paulista)</a></p>
-      </address>
+    <>
 
-      <hr />
-      <h5>Endereço:</h5>
-      <iframe
+    <Container fluid align='center'>
+        <Div>
+          <Row md={2}>
+            <Col>
+              <Title>Entre em Contato</Title>
+              <Text><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"/>
+  <path fill-rule="evenodd" d="M16 .5a.5.5 0 0 0-.5-.5h-5a.5.5 0 0 0 0 1h3.793L6.146 9.146a.5.5 0 1 0 .708.708L15 1.707V5.5a.5.5 0 0 0 1 0v-5z"/>
+</svg> <a href="mailto:infinity.ifpe@gmail.com">Clique aqui</a> ou escaneie o QR-Code ao lado e fale com nossos consultores!
+              <br />
+              Por favor nos informe:
+              <br />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg> Nome
+              <br />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+</svg> E-mail
+              <br />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+  <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+</svg> WhatsApp
+              <br />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-link-45deg" viewBox="0 0 16 16">
+  <path d="M4.715 6.542 3.343 7.914a3 3 0 1 0 4.243 4.243l1.828-1.829A3 3 0 0 0 8.586 5.5L8 6.086a1.002 1.002 0 0 0-.154.199 2 2 0 0 1 .861 3.337L6.88 11.45a2 2 0 1 1-2.83-2.83l.793-.792a4.018 4.018 0 0 1-.128-1.287z"/>
+  <path d="M6.586 4.672A3 3 0 0 0 7.414 9.5l.775-.776a2 2 0 0 1-.896-3.346L9.12 3.55a2 2 0 1 1 2.83 2.83l-.793.792c.112.42.155.855.128 1.287l1.372-1.372a3 3 0 1 0-4.243-4.243L6.586 4.672z"/>
+</svg> Site da sua Empresa ou do seu Projeto
+              <br />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-card-text" viewBox="0 0 16 16">
+  <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z"/>
+  <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z"/>
+</svg> Descrição do seu Projeto, cite exemplos semelhantes
+              <br />
+              </Text>
+            </Col>
+            <Col md='auto'>
+            <a href="mailto:infinity.ifpe@gmail.com"><Image src={qrcode} /></a></Col>
+          </Row>
+        </Div>
+
+    <Div2>
+          <Row md={2}>
+            <Col md='auto'><iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.515056446582!2d-34.861024148546555!3d-7.94560588132668!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ab3dbf4a9cd5f1%3A0x45bf1641ad29a799!2sIFPE-%20Intituto%20Federal%20De%20Pernambuco%20%2C%20Paulista!5e0!3m2!1spt-BR!2sbr!4v1650408729027!5m2!1spt-BR!2sbr"
-        width="800"
+        width="600"
         height="450"
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      ></iframe></Col>
+            <Col>
+              <Title>Endereço</Title>
+              <Text><b>Instituto Federal de Pernambuco <i>(Campus Paulista)</i></b>
+              <address> Av. Prefeito Geraldo Pinho Alves, nº 1.400, Maranguape I, CEP: 53441-600<br />
+              Paulista-PE
+              <br />
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+  <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+  <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+</svg>  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-map" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M15.817.113A.5.5 0 0 1 16 .5v14a.5.5 0 0 1-.402.49l-5 1a.502.502 0 0 1-.196 0L5.5 15.01l-4.902.98A.5.5 0 0 1 0 15.5v-14a.5.5 0 0 1 .402-.49l5-1a.5.5 0 0 1 .196 0L10.5.99l4.902-.98a.5.5 0 0 1 .415.103zM10 1.91l-4-.8v12.98l4 .8V1.91zm1 12.98 4-.8V1.11l-4 .8v12.98zm-6-.8V1.11l-4 .8v12.98l4-.8z"/>
+</svg>
+              </address>
+              </Text>
+            </Col>
+          </Row>
+        </Div2>
+
     </Container>
+    </>
   )
 }
 
--- frontend/src/components/map/styles.js
+++ frontend/src/components/map/styles.js
@@ -1,5 +1,30 @@
 import styled from 'styled-components'
 
+export const Image = styled.img`
+  width: 50%;
+  padding-top: 4.8em;
+  padding-bottom: 4.8em;
+`
+export const Title = styled.h1`
+  padding-top: 1.5em;
+  font-size: 3.2em;
+  font-weight: bold;
+  text-align: center;
+  color: ${props => props.theme.colors.text};
+`
+export const Text = styled.p`
+  padding-bottom:1.5em;
+  text-align: center;
+  color: ${props => props.theme.colors.text};
+`
+export const Div = styled.div`
+  background-image: linear-gradient(to left, ${props => props.theme.colors.background} 50%);
+  padding-left: 7%;
+`
+export const Div2 = styled.div`
+  background-image: linear-gradient(to left, ${props => props.theme.colors.background}, #00A6ED, #00A6ED, ${props => props.theme.colors.background});
+  padding-left: 7%;
+`
 export const Container = styled.div`
 
 `
