d35718c0ad85835b6301480d849d857343be4134
ArthurVBS <arthurvini2703@gmail.com>
2022-07-17 16:02:02 -0300

    Fix responsivity on about page

--- frontend/src/pages/about/index.jsx
+++ frontend/src/pages/about/index.jsx
@@ -2,24 +2,28 @@ import React from 'react'
 import Footer from '../../components/footer'
 import Header from '../../components/header'
 import Astronaut from '../../assets/astronaut-about.png'
-import { Container, Text, Image, Title } from './styles'
-import { Row, Col } from 'react-bootstrap'
+import { Container, Text, Image, Title, LeftBox, RightBox } from './styles'
+
 const About = () => {
   return (
     <>
       <Header />
 
-      <Container align='center'>
-
-        <Row md={2}>
-          <Col>
-          <Title>Quem Somos</Title>
-          <Text>A Infinity é uma empresa especializada no desenvolvimento de sistemas inovadores. Para nós, desenvolver sistemas é a arte de transformar ideias em um negócio de sucesso. Com uma equipe altamente criativa, competente e qualificada para tornar seus sonhos em realidade. Fazendo parte da história de mais de 500 clientes, faremos a diferença na sua história!</Text>
-          </Col>
-        <Col>
-        <Image src={Astronaut} />
-        </Col>
-        </Row>
+      <Container>
+        <LeftBox>
+          <Title>Quem Somos?</Title>
+          <Text>
+            A Infinity é uma empresa especializada no desenvolvimento de
+            sistemas inovadores. Para nós, desenvolver sistemas é a arte de
+            transformar ideias em um negócio de sucesso. Com uma equipe
+            altamente criativa, competente e qualificada para tornar seus sonhos
+            em realidade. Fazendo parte da história de mais de 500 clientes,
+            faremos a diferença na sua história!
+          </Text>
+        </LeftBox>
+        <RightBox>
+          <Image src={Astronaut} />
+        </RightBox>
       </Container>
 
       <Footer />
--- frontend/src/pages/about/styles.js
+++ frontend/src/pages/about/styles.js
@@ -1,30 +1,50 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  background-image: linear-gradient(
+    to left,
+    ${props => props.theme.colors.background},
+    #00a6ed 70%
+  );
+  color: ${props => props.theme.colors.text};
   display: flex;
   justify-content: center;
-  gap: 50px;
-  padding: 2%;
-  padding-bottom: 20%;
-  background-image: linear-gradient(to left, ${props => props.theme.colors.background}, #00A6ED 70%);
-  color: ${props => props.theme.colors.text};
+  gap: 16px;
+  padding: 8px;
 
-  @media screen and (max-width: 828px){
-    flex-direction:column;
+  @media screen and (max-width: 828px) {
+    flex-direction: column;
   }
 `
 
+export const LeftBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  padding: 8px;
+`
+
+export const RightBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-width: 40%;
+`
+
 export const Image = styled.img`
-  padding-top: 2%;
-  padding-left: 18%;
+  width: 100%;
+  max-width: 400px;
 `
 
 export const Title = styled.h1`
-
+  letter-spacing: 1px;
+  font-size: 1.8em;
+  font-weight: 700;
 `
 
 export const Text = styled.p`
+  text-indent: 2em;
   text-align: justify;
   padding-top: 5px;
-  font-size: 1.5em;
+  font-size: 1.3em;
 `
